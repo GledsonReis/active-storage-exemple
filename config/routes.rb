39f15c2f2b3dfbcb2f3f8851f0b8c0257ab4d5d2
Gledson Reis <gledsonthegreeder@gmail.com>
2020-02-12 10:08:02 -0300

    bucket CRUD_file imagemagick

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'buckets#index'
+  resources :buckets do
+    resources :file_uploads, only: %i[new create destroy]
+  end
 end
